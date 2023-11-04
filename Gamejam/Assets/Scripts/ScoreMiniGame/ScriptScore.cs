d9ebb8aaf82f61b1e24e8904b0ed21a2e416b725
vanya5987 <vanya5987@Mail.ru>
2023-11-04 18:13:40 +0300

    ScoreTabl

---
+++ Gamejam/Assets/Scripts/ScoreMiniGame/ScriptScore.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScriptScore : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI score;
+    [SerializeField] private int countScoreTabl = 0;
+    void Start()
+    {
+    }
+    public void AddScore()
+    {
+        countScoreTabl++;
+        score.text = "—чет " + Convert.ToString(countScoreTabl);
+    }
+}
